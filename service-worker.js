2e20127ae66ef588482acba002c57c4f0190795a
Michael Petrosyan <mikpetr95@gmail.com>
2017-05-21 21:16:06 +0400

    Added asstets files

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,16 @@ this.addEventListener('install', function (event) {
   event.waitUntil(caches.open('spotify-client-v1').then(function (cache) {
     return cache.addAll([
       '/index.html',
-      '/assets/',
       '/vendor.bundle.js',
-      '/app.bundle.js'
+      '/app.bundle.js',
+      '/assets/fonts/Raleway-Medium.ttf',
+      '/assets/fonts/Raleway-Regular.ttf',
+      '/assets/fonts/Raleway-SemiBold.ttf',
+      '/assets/images/Magnify@2x.png',
+      '/assets/images/Page 1@2x.png',
+      '/assets/images/gl-logo.png',
+      '/assets/images/gl-text.png',
+      '/assets/images/hero-img@2x.png'
     ]);
   }));
 });
